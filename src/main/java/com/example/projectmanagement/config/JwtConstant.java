3c7c5f39941a54dc623b7328d08ce962a05cb854
saurabh <saurabh@saurabhs-MacBook-Air.local>
2024-06-05 13:12:35 +0530

    just checkinh

--- src/main/java/com/example/projectmanagement/config/JwtConstant.java
+++ src/main/java/com/example/projectmanagement/config/JwtConstant.java
@@ -3,4 +3,5 @@ package com.example.projectmanagement.config;
 public class JwtConstant {
     public static final String SECRET_KEY = "secret--GERGERER";
     public static final String JWT_HEADER = "Authorization";
+
 }
